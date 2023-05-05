995b3f0fd7803be13ca8a43944c13efcb38122d7
monktar <monktar@gmail.com>
2023-05-04 20:25:06 -0500

    Update bello_postgres_multi_install.sql

--- bello_postgres_multi_install.sql
+++ bello_postgres_multi_install.sql
@@ -79,6 +79,6 @@ Learn PostgreSQL
 Luca Ferrari, Enrico Pirozzi
 
 Getting to Know Your Cluster
-		Exploring the disk layout of PGDATA
+		Objects in the PGDATA directory
 		
 2/6/2023 config files, install new postgresql, run command for diff versions of postgresql hosted:start, stop, reload, postgresql processes
