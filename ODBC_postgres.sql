1a2954c93c58f5de74d4460ae366ca8d67a04191
monktar <monktar@gmail.com>
2023-06-05 14:20:23 -0500

    Update ODBC_postgres.sql

--- ODBC_postgres.sql
+++ ODBC_postgres.sql
@@ -2,3 +2,6 @@ https://www.postgresql.org/ftp/odbc/versions/msi/
 
 use PostgreSQL Unicode (x64) with sql server 2017
 
+product name:postgresql
+Provider: Microsoft OLE DB Provider ddor ODBC Drivers
+
