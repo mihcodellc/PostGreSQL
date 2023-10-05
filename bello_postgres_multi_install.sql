71d8d7c2806a05a7dae6a4791c0e24a0bb42cf0a
monktar <monktar@gmail.com>
2023-10-04 23:39:27 -0500

    Update bello_postgres_multi_install.sql

--- bello_postgres_multi_install.sql
+++ bello_postgres_multi_install.sql
@@ -114,7 +114,7 @@ PostgreSQL has three types of tables:
 	-Unlogged tables: Very fast tables to be used as support tables common to all users
 	-Logged tables: Regular tables	
 
-	next "Managing temporary tables"
+	next "Windows Functions"
 Learn PostgreSQL
 Luca Ferrari, Enrico Pirozzi
 
